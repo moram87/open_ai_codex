a659976111b4f3c36fddacb440907a414a581261
moram87 <marc.oram87@gmail.com>
2023-04-21 15:45:31 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the server -> bot's response
   
-  const response = await fetch('https://codex-7bq6.onrender.com', {
+  const response = await fetch('https://codex7bq6.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
