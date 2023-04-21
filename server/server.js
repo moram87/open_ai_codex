8e5879e6f43db0449b7559831545fd51265afa63
moram87 <marc.oram87@gmail.com>
2023-04-21 16:09:35 -0500

    server updates

--- server/server.js
+++ server/server.js
@@ -43,5 +43,5 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(5000, () => {
-    console.log('Server is running on port http://localhost:5000');
+    console.log('Server is running on port https://codex-7bq6.onrender.com/');
 });
